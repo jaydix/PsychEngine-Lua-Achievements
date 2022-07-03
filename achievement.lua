704596d53c609bd2b6ebd4c524b7a88aee3ab98e
8Bit <75950907+8bitjake@users.noreply.github.com>
2022-07-03 15:11:30 -0500

    backend already checks for achievement unlocked

--- achievement.lua
+++ achievement.lua
@@ -3,7 +3,7 @@ local allowEnd = false
 function onEndSong()
     if (not botPlay) then
         -- Block the first countdown and start a timer of 0.8 seconds to play the dialogue
-        if (not allowEnd) and (misses == 0) and (not getDataFromSave('achievements', 'achievement-tag', false)) then
+        if (not allowEnd) and (misses == 0) then
             setProperty('inCutscene', true);
             callOnLuas('unlockAchievement', {"achievement-tag", getPropertyFromClass('Paths', 'currentModDirectory'), true})
             allowEnd = true;
